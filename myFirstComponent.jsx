fbab1e506774f0f3b4f4d9d8473a27c7df297ef2
atita <anaguasoft@gmail.com>
2018-06-27 21:09:09 +0300

    My First React component

---
+++ myFirstComponent.jsx
@@ -0,0 +1,7 @@
+// change code below this line
+class MyComponent extends React.Component  {
+  render() {
+    return   <h1>My First React Component!</h1>
+  } 
+}
+ReactDOM.render(<MyComponent/>,document.getElementById('challenge-node'));
