34be2fa0fe90a8c126d9585c2c0b5481957107dc
atita <anaguasoft@gmail.com>
2018-06-29 14:03:27 +0300

    Stateless function components review :wrench:

---
+++ statelessFunctionalReview.jsx
@@ -0,0 +1,21 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// change code below this line
+const  Camper =(props)=> {
+  return (
+    <p>{props.name}</p>
+  )
+};
+
+Camper.propTypes = { name : PropTypes.string.isRequired }
+Camper.defaultProps = {name:'CamperBot'}
